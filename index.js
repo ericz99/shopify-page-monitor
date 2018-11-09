03d9147fab779c5d6bcb582c1436a984dd244006
Eric Zhang <ericzhang128@gmail.com>
2018-11-09 12:20:56 -0500

    gucci gang

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ require("console-stamp")(console, {
     metadata: "green"
   }
 });
+
+const request = require("request-promise");
+const xml2js = require("xml2js");
+const colors = require("colors");
+const cheerio = require("cheerio");
+const moment = require("moment");
+const fs = require("fs");
+const path = require("path");
+const config = require(path.join(__dirname, "config.json"));
+
+const formattedProxies = [];
+
+function formatProxy(proxy) {
+  if (proxy && ["localhost", ""].indexOf(proxy) < 0) {
+    let splitProxy = proxy.split(":");
+    if (splitProxy.length > 3) {
+    }
+  }
+}
